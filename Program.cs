78c88744cdb9c9d3a18bc2ed96f9dedff6690616
abdal-security-group <abdal.group@gmail.comgit>
2023-10-03 04:15:33 +0330

    Abdal Web Visitor

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        // Get Software Version and Change Title
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+        Console.Title = "Abdal Web Visitor :: Ver " + version;
+
+        Console.WriteLine("");
+        Console.WriteLine("Programmer: Ebrahim Shafiei (EbraSha)");
+        Console.WriteLine("Email: Prof.Shafiei@Gmail.com");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("##################################");
+        Console.WriteLine("");
+        // Read settings
+        var settingsXml = XDocument.Load("settings.xml");
+        bool infinite = bool.Parse(settingsXml.Root.Element("Infinite").Value);
+        int loadWaitTime = int.Parse(settingsXml.Root.Element("LoadWaitTime").Value);
+        bool enableScroll = bool.Parse(settingsXml.Root.Element("EnableScroll").Value);
+        int scrollWaitTime = int.Parse(settingsXml.Root.Element("ScrollWaitTime").Value);
+        int idleTimeAfterScroll = int.Parse(settingsXml.Root.Element("IdleTimeAfterScroll").Value);
+
+        // Read URLs
+        var urlsXml = XDocument.Load("urls.xml");
+        List<string> urls = urlsXml.Root.Elements("url").Select(x => x.Value).ToList();
+
+        // Open browser
+        IWebDriver driver = new ChromeDriver(".");
+
+        try
+        {
+            do
+            {
+                foreach (var url in urls)
+                {
+                    driver.Navigate().GoToUrl(url);
+                    Thread.Sleep(loadWaitTime);
+
+                    if (enableScroll)
+                    {
+                        // Scroll to the middle of the page
+                        ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight/2)");
+
+                        Thread.Sleep(scrollWaitTime);
+
+                        // Scroll to the end of the page
+                        ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
+
+                        Thread.Sleep(idleTimeAfterScroll);
+                    }
+                }
+            } while (infinite);
+        }
+        finally
+        {
+            driver.Quit();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# abdal-web-visitor
-Abdal Web Visitor" is a powerful tool for automated website visits using XML configurations. 
+# Abdal Web Visitor
+
+![](https://raw.githubusercontent.com/ebrasha/Abdal-Eni-Benchmark/main/Abdal-Eni-Benchmark.jpg)
+
+# 🤵 Programmer
+Ebrahim Shafiei (EbraSha)
+
+Email :  Prof.Shafiei@Gmail.com
+
+
+## 🤞 Cooperation
+If you, like me, are passionate about open-source projects and helping people worldwide, please reach out so we can collaborate to build a better world.
+
+
+## README Translation
+- [English](README.md)
+- [فارسی](README.fa.md)
+
+## 💎 General purpose
+"Abdal Web Visitor" is a powerful tool for automated website visits using XML configurations.
+
+## 📄 Description
+Abdal Web Visitor by Ebrahim Shafiei: An automated tool designed to simulate real-user interactions by visiting and scrolling web pages as per the configured settings. Perfect for SEO and web testing purposes. Configure via XML. Written in .NET 6 with Selenium. 🌐🤖📊
+
+
+# ✨ Features
+- Without any malicious code and spyware
+-  Automated web page visiting.
+-  Simulated user-like scrolling actions.
+-  Infinite loop feature for continuous page visits.
+-  Configurable page load wait times.
+-  Configurable scroll wait times.
+-  XML-based configuration for URLs and settings.
+-  Built on .NET 6 with Selenium.
+-  Cross-browser support with ChromeDriver.
+-  Easy setup and user-friendly design.
+-  Ideal for SEO, web testing, and monitoring purposes.
+- Completely open source
+
+
+# 💡 Installation
+
+#### For all operating systems
+Just download and run the version you desire.
+ 
+## ✅ Supported operating systems
+
+- Win 10 x64
+- Win 7 x64
+- debian.11-x64
+- debian.11-arm64
+- ubuntu.22.04-x64
+- ubuntu.22.04-arm64
+- centos.7-x64
+- fedora.37-x64
+
+ 
+
+## ⚠️ Legal disclaimer ⚠️
+
+Use this tool only for legal purposes and with permission. The developer assumes no liability for any illicit usage or harm inflicted upon others as a result of using this tool.
+
+## ❤️ Donations to the project
+
+> USDT:      TKRmTvwkMFdgGrwvDHztLXsKAwK2WEEKUp
+
+> bitcoin:   bc1q9w9ymgz2wluax60rsuza4q0at7gpy82g8el6zj
+
+> For Iranian People -> MellatBank : 6104-3378-5301-4247
+
+## ❗ Reporting Issues
+
+If you are facing a configuration issue or something is not working as you expected to be, please use the Prof.Shafiei@Gmail.com . Issues on GitLab are also welcomed.
+
